ab82cfeee0d44046f8810178df108a5491dc97bc
Naga Srinivas Challagandla <naga1729@gmail.com>
2013-08-01 21:57:36 +0530

    index.html modified to the basic version using bootstrap

--- grader.js
+++ grader.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-/* 
+/*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
@@ -14,7 +14,7 @@ References:
     - https://github.com/visionmedia/commander.js
     - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
-  + JSON 
+  + JSON
     - http://en.wikipedia.org/wiki/JSON
     - https://developer.mozilla.org/en-US/docs/JSON
     - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -41,6 +42,17 @@ var cheerioHtmlFile = function(htmlfile){
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioUrl = function(url){
+    rest.get(url).on('complete', function(result, response){
+      if(result instanceof Error){
+	console.error('Error: ' + util.format(response.message));
+      }else{
+	console.log("Validating the URL mentioned..");
+	return cheerio.load(result);
+      }});
+
+};
+
 var loadChecks = function(checksfile){
     return JSON.parse(fs.readFileSync(checksfile));
 };
@@ -56,18 +68,35 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function(url, checksfile) {
+    $ = cheerioUrl(url);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks){
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_link>', 'URL address')
+	.parse(process.argv);
+    if(program.file){
+	var checkJson = checkHtmlFile(program.file, program.checks);
+    }else if(program.url){
+	var checkJson = checkUrl(program.url, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
